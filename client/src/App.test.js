ae0e85b5d465cb755e60649938254f419f955571
walter <buczacka.walter@gmail.com>
2021-10-11 07:54:41 -0300

    update test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -6,8 +6,8 @@ import {render, screen} from '@testing-library/react'
 test('render home page successfully', () => {
   const Wrapper = ({children}) => <AppProviders>{children}</AppProviders>
   render(<App />, {wrapper: Wrapper})
-  const message = screen.getByText(/welcome home/i)
-  const realTimeMessage = screen.getByText(/real time/i)
-  expect(message).toHaveTextContent('Welcome Home')
+  const message = screen.getByText(/welcome to city bikes/i)
+  const realTimeMessage = screen.getByText(/real time bikes/i)
+  expect(message).toHaveTextContent('Welcome to City Bikes')
   expect(realTimeMessage).toHaveTextContent('Real Time bikes')
 })
